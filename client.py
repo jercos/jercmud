f8678e551234ca8dfd2f458f8e5ec6c8df2fdece
Jeremy S <jercos@gmail.com>
2012-06-02 21:04:21 -0700

    Updated some datums, routed some paths

--- client.py
+++ client.py
@@ -4,10 +4,16 @@ import sqlite3
 import readline
 import signal
 import sys
+import time
+from getpass import getpass
+from hashlib import sha256
 conn = sqlite3.connect('/tmp/mud-testing')
-c=conn.cursor()
+
 globalid = 0
+player = None
+
 pollrate = 2
+
 
 PROMPT = '> '
 
@@ -16,7 +22,8 @@ def process(sig,stack):
 	if sig != signal.SIGALRM:
 		print("Something's up. Expected signal",signal.SIGALRM,"but got",sig)
 		raise
-	c.execute("SELECT id,type,text FROM globalevents WHERE id > ?",(globalid,))
+	c = conn.cursor()
+	c.execute("SELECT id,type,text,strftime('%s',time) FROM globalevents WHERE id > ?",(globalid,))
 	result = c.fetchone()
 	if result == None:
 		signal.alarm(pollrate)
@@ -24,11 +31,56 @@ def process(sig,stack):
 	print()
 	while result != None:
 		globalid = result[0]
-		print("Event type '%s': '%s'"%(result[1],result[2]))
+		print("[%s] Event type '%s': '%s'"%(time.ctime(float(result[3])),result[1],result[2]))
 		result = c.fetchone()
 	signal.alarm(pollrate)
 	print(PROMPT, readline.get_line_buffer(), sep='', end='')
 	sys.stdout.flush()
+	c.close()
+
+def globalevent(type,text):
+	c = conn.cursor()
+	c.execute("INSERT INTO globalevents (type, text, time) VALUES (?,?,datetime('now'))",(type,text,))
+	conn.commit()
+	c.close()
+
+def command(text):
+	global player
+	c = conn.cursor()
+	
+	c.close()
+
+c = conn.cursor()
+while player == None:
+	username = raw_input("Username: ")
+	if username == "new":
+		username = raw_input("New username: ")
+		c.execute("SELECT name FROM users WHERE name = ?",(username,))
+		if c.fetchone() != None:
+			print("Sorry, that username is taken.")
+			continue
+		passhash = sha256(getpass("New password: ")).hexdigest()
+		if passhash != sha256(getpass("Confirm password: ")).hexdigest():
+			print("Passwords didn't match.")
+			continue
+		try:
+			c.execute("INSERT INTO users (name,pass) VALUES (?,?)",(username,passhash,))
+		except IntegrityError:
+			print("Database collision... Someone snuck in and grabbed it while you were typing?")
+			continue
+	passhash = sha256(getpass("Password: ")).hexdigest()
+	print("Logging in as",username,"with password",passhash)
+	c.execute("SELECT id,name,pass FROM users WHERE name = ? AND pass = ?",(username,passhash,))
+	result = c.fetchone()
+	if result == None:
+		print("Sorry, that's not a valid login.")
+		print("If you want to make an account, log in as 'new'")
+		continue
+	player = dict(zip(("id","name","pass"),result))
+globalevent('login',"%s logged in"%player['name'])
+conn.commit()
+c.close()
+print(player)
 
 while True:
 	try:
--- daemon.py
+++ daemon.py
@@ -5,11 +5,11 @@ conn = sqlite3.connect('/tmp/mud-testing')
 (lambda c:
 	c.executescript("""
 BEGIN;
-CREATE TABLE IF NOT EXISTS globalevents (id INTEGER PRIMARY KEY, type, text);
-CREATE TABLE IF NOT EXISTS roomevents (id INTEGER PRIMARY KEY, type, text);
-CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY, name, description);
+CREATE TABLE IF NOT EXISTS globalevents (id INTEGER PRIMARY KEY, type, text, time);
+CREATE TABLE IF NOT EXISTS roomevents (id INTEGER PRIMARY KEY, type, text, time);
+CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY, name, description, owner);
 CREATE TABLE IF NOT EXISTS exits (id INTEGER PRIMARY KEY, src, dest, name, description);
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name, pass);
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name UNIQUE, pass);
 COMMIT;
 """)
 )(conn.cursor())
@@ -18,6 +18,6 @@ while True:
 	time.sleep(10)
 	c=conn.cursor()
 	print "Inserting tone, "+time.asctime()
-	c.execute("INSERT INTO globalevents (type, text) VALUES ('time',?);",("At the tone, the time will be: "+time.asctime(),))
+	c.execute("INSERT INTO globalevents (type, text, time) VALUES ('time',?,datetime('now'));",("At the tone, the time will be: "+time.asctime(),))
 	c.close()
 	conn.commit()
